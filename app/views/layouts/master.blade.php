69ccd37a10958fe464cb5ca0b3471333e59f3a47
haydogsup <joshua.r.mays@gmail.com>
2014-10-03 15:43:47 -0500

    changed roll-dice to use blade templates

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>I'm a title</title>
+    <!-- Include Boot Strap -->
+    <!-- Include JQuery -->
+</head>
+<body>
+    
+    <!-- STANDARD HEADING -->
+    <!-- NAV BAR -->
+    @yield('content')
+    
+    <!-- STANDAR FOOTER -->
+    
+</body>
+</html>
