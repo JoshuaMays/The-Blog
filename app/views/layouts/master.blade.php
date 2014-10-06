b86e185c96cd6ef5b6cf9d5206d66e0c56545b1a
haydogsup <joshua.r.mays@gmail.com>
2014-10-06 13:32:44 -0500

    added template content and custom css

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Joshua Mays">
-    <title>BLAWG</title>
+    <title>@yield('title')</title>
 
     <!-- Bootstrap CSS -->
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- Freelancer Theme CSS -->
     <link href="/css/freelancer.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="/css/style.css" rel="stylesheet">
     <!-- Fonts -->
     <link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
@@ -24,8 +26,55 @@
     <![endif]-->
 </head>
 <body>
-
-    @yield('content')
+    <div id="wrap">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle grouped for better mobile display -->
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#page-top">ERMAGERD</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="{{{ action('HomeController@showWelcome') }}}">Home</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="{{{ action('HomeController@showPortfolio') }}}">Portfolio</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="{{{ action('HomeController@showResume') }}}">Resume</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="/">Blog</a>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div id="contentWrap" class="container">
+        
+        @yield('content')
+        
+        </div>
+    </div><!-- /#pageWrap -->
+    <div id="push"></div>
+    </div>
+    <footer id="footer" class="text-center">
+        <div class="container">
+            <p class="muted credit">&copy; 2014 <a href="/index.html">Joshua Mays</a></p>
+        </div>
+    </footer>
 
     <!-- jQuery -->
     <script src="//code.jquery.com/jquery.js"></script>
