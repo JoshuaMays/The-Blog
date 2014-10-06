2e22cfea94e1e63a5bed6e4d1a381f1858e1cd4a
haydogsup <joshua.r.mays@gmail.com>
2014-10-06 10:01:07 -0500

    added template and css/fonts to master template

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,16 +1,35 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>I'm a title</title>
-    <!-- Include Boot Strap -->
-    <!-- Include JQuery -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Joshua Mays">
+    <title>BLAWG</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Freelancer Theme CSS -->
+    <link href="/css/freelancer.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-    
-    <!-- STANDARD HEADING -->
-    <!-- NAV BAR -->
+
     @yield('content')
-    
-    <!-- STANDAR FOOTER -->
-    
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
