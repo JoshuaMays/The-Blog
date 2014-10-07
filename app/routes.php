ed47b9c9861f3c4224f52806f073f4b9110ec9e1
haydogsup <joshua.r.mays@gmail.com>
2014-10-07 10:35:50 -0500

    Removed: rolldice. Added: Resource Controller for Posts

--- app/routes.php
+++ app/routes.php
@@ -17,27 +17,6 @@ Route::get('/resume', 'HomeController@showResume');
 
 Route::get('/portfolio', 'HomeController@showPortfolio');
 
-Route::get('/rolldice/{guess}', function($guess)
-{
-    // GENERATE RANDOM NUMBER
-    $randomNumber = mt_rand(1,6);
-
-    if ($guess == $randomNumber) {
-        $match = "WHOA YOU GUESSED IT. YOU MUST BE A SAVANT!";
-    }
-    else {
-        $match = "Wrong. Not even close. And there were only 6 numbers to choose from. Guess better.";
-    }
-    // ARRAY TO HOLD DATA TO PASS TO VIEW
-    $data = [
-        'guess' => $guess,
-        'randomNumber' => $randomNumber,
-        'match' => $match
-    ];
-
-    return View::make('roll-dice', $data);
-});
-
 Route::get('orm-test', function()
 {
     $post = Post::find(2);
@@ -45,3 +24,5 @@ Route::get('orm-test', function()
     $post->delete();
 
 });
+
+Route::resource('posts', 'PostsController');
