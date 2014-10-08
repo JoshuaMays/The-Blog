f8fb9db311995c56a465bdc58dab1d7d486d9015
haydogsup <joshua.r.mays@gmail.com>
2014-10-08 10:22:09 -0500

    replaced html form with larval produced form and form elements

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -7,19 +7,25 @@
 @section('content')
     <div class="col-md-8 col-md-offset-2">
         <h1>Create a New Post</h1>
-        <form action="{{{ action('PostsController@store') }}}" method="POST" role="form">
-        
+        {{ Form::open(array('action' => 'PostsController@store')) }}
+            
             <div class="form-group">
-                <label for="">Title</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{{ Input::old('title') }}}" placeholder="Enter Blog Post Title">
+                {{ Form::label('title', 'Post Title') }}
+                {{ Form::text('title', Input::old('title'), array('class' =>'form-control', 'placeholder' => "Enter Blog Post Title")) }}
+                
+                {{ $errors->first('title', '<span class="alert alert-danger alert-dismissible help-block"><span class="icon-uniE604"></span><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
             </div>
             
             <div class="form-group">
-                <label for="content">Body</label>
-                <textarea class="form-control" name="content" id="content" rows="15" placeholder="Your blog post text here">{{{ Input::old('content') }}}</textarea>
+                {{ Form::label('content', 'Content') }}
+                {{ Form::textarea('content', Input::old('content'), array('class' => 'form-control', 'placeholder' => 'Your blog post text here', 'rows' => '15')) }}
+                
+                {{ $errors->first('content', '<span class="alert alert-danger alert-dismissible help-block"><span class="icon-uniE604"></span><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
             </div>
             
-            <button type="submit" class="btn btn-md btn-outline">SUBMIT</button>
-        </form>
+            {{ Form::submit('SUBMIT', array('class' => "btn btn-md btn-outline")) }}
+            
+            <!-- <button type="submit" class="btn btn-md btn-outline">SUBMIT</button> -->
+        {{ Form::close() }}
     </div>
 @stop
