fd3507ad7326226cd405c4f26724c2ea3d5a5bbc
haydogsup <joshua.r.mays@gmail.com>
2014-10-08 10:21:22 -0500

    added required and max length rules to post object

--- app/models/post.php
+++ app/models/post.php
@@ -2,5 +2,9 @@
 
 class Post extends Eloquent
 {
+    public static $rules = array(
+        'title' => 'required|max:255',
+        'content' => 'required'
+    );
     protected $table = 'posts';
 }
