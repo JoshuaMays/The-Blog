f3b7600586261595203b6b7a4e26d26aa8cf1f3a
haydogsup <joshua.r.mays@gmail.com>
2014-10-09 08:38:20 -0500

    moved form to form.blade, added clear button.

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,31 +1,20 @@
 @extends('layouts.master')
 
 @section('title')
-    Create A Post
+    Create a New Post
 @stop
 
 @section('content')
     <div class="col-md-8 col-md-offset-2">
-        <h1>Create a New Post</h1>
+        <h1>@yield('title')</h1>
         {{ Form::open(array('action' => 'PostsController@store')) }}
+
+        @include('posts.form')
             
-            <div class="form-group">
-                {{ Form::label('title', 'Post Title') }}
-                {{ Form::text('title', Input::old('title'), array('class' =>'form-control', 'placeholder' => "Enter Blog Post Title")) }}
-                
-                {{ $errors->first('title', '<span class="alert alert-danger alert-dismissible help-block"><span class="icon-uniE604"></span><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
-            </div>
-            
-            <div class="form-group">
-                {{ Form::label('content', 'Content') }}
-                {{ Form::textarea('content', Input::old('content'), array('class' => 'form-control', 'placeholder' => 'Your blog post text here', 'rows' => '15')) }}
-                
-                {{ $errors->first('content', '<span class="alert alert-danger alert-dismissible help-block"><span class="icon-uniE604"></span><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
-            </div>
-            
-            {{ Form::submit('SUBMIT', array('class' => "btn btn-md btn-outline")) }}
-            
-            <!-- <button type="submit" class="btn btn-md btn-outline">SUBMIT</button> -->
+        
+        {{ Form::reset('CLEAR', array('class' => 'btn btn-md btn-outline')) }}
+        {{ Form::submit('SUBMIT', array('class' => "btn btn-md btn-outline")) }}
+
         {{ Form::close() }}
     </div>
 @stop
