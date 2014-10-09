84566938a682bf986dcf1026fe39da0b85925e19
haydogsup <joshua.r.mays@gmail.com>
2014-10-09 08:42:33 -0500

    added post created_at date. changed a links to link_to_action helpers

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -5,14 +5,18 @@
 @stop
 
 @section('content')
-    <div class="container contentWrapper">
         <div class="row">
             <div class="col-sm-10 col-md-10 col-sm-offset-1 col-md-offset-1 well">
                 <article>
                     <h2>{{{ $post->title }}}</h2>
                     <p>{{{ $post->content }}}</p>
+                    <p><small>{{{ $post->created_at->setTimezone('America/Chicago')->format(Post::DATE_FORMAT) }}}</small></p>
                 </article>
+                {{ link_to_action('PostsController@edit', "EDIT", $post->id, array('class' => 'btn btn-xs btn-outline', 'role' => 'button')) }}
             </div>
         </div>
-    </div>
+@stop
+
+@section('footer')
+    {{ link_to_action('PostsController@index', 'BACK', null, array('id' => 'indexLink', 'class' => 'btn btn-xs btn-outline')) }}
 @stop
