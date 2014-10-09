b015dba240304b40003b0bc1901df319322faea8
haydogsup <joshua.r.mays@gmail.com>
2014-10-09 08:36:15 -0500

    added DATE_FORMAT const. adding tagging

--- app/models/post.php
+++ app/models/post.php
@@ -2,9 +2,15 @@
 
 class Post extends Eloquent
 {
+    use Conner\Tagging\TaggableTrait;
+    
+    const DATE_FORMAT = 'l, jS \\of F Y g:i A';
+
     public static $rules = array(
         'title' => 'required|max:255',
         'content' => 'required'
     );
     protected $table = 'posts';
+    
+    protected $fillable = ['title', 'content'];
 }
