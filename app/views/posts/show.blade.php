2a6f107417e06dec725b1dd91ba45136098b8ef2
haydogsup <joshua.r.mays@gmail.com>
2014-10-10 11:23:05 -0500

    removed setTimezone from date format display. Added delete form button.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -10,13 +10,28 @@
                 <article>
                     <h2>{{{ $post->title }}}</h2>
                     <p>{{{ $post->content }}}</p>
-                    <p><small>{{{ $post->created_at->setTimezone('America/Chicago')->format(Post::DATE_FORMAT) }}}</small></p>
+                    <p><small>{{{ $post->created_at->format(Post::DATE_FORMAT) }}}</small></p>
                 </article>
-                {{ link_to_action('PostsController@edit', "EDIT", $post->id, array('class' => 'btn btn-xs btn-outline', 'role' => 'button')) }}
+                    {{ link_to_action('PostsController@edit', "EDIT", $post->id, array('class' => 'btn btn-xs btn-outline pull-left', 'role' => 'button')) }}
+                    {{ Form::open(array('action' => ['PostsController@destroy', $post->id], 'method' => "delete", 'id' => 'deleteForm')) }}
+                        {{ Form::submit('DELETE',['class'=>'btn btn-xs btn-omg btn-outline pull-left']) }}
+                    {{ Form::close() }}
             </div>
         </div>
 @stop
 
 @section('footer')
     {{ link_to_action('PostsController@index', 'BACK', null, array('id' => 'indexLink', 'class' => 'btn btn-xs btn-outline')) }}
+@stop
+
+@section('bottom-script')
+    <script type="text/javascript">
+        $("#deleteForm").submit(function(e)
+        {
+            if (!confirm('Are you sure?')) {
+                e.preventDefault();
+            }
+
+        });
+    </script>
 @stop
