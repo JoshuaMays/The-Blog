c1b62bdb53af21adaf6719fc50cc68cf29feb512
haydogsup <joshua.r.mays@gmail.com>
2014-10-10 11:09:26 -0500

    testing session::flash

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,6 +17,9 @@ class HomeController extends BaseController {
 
     public function showWelcome()
     {
+        // set a value in the session
+        Session::flash('key', 'lolol');
+
         return View::make('hello');
     }
 
