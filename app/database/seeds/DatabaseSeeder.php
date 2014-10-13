27ea2f3baffc8962309e9eca764b93814b504497
haydogsup <joshua.r.mays@gmail.com>
2014-10-13 14:30:13 -0500

    adding projects table seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,17 +2,18 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
 
-		$this->call('UsersTableSeeder');
-		$this->call('PostsTableSeeder');
-	}
+        $this->call('UsersTableSeeder');
+        $this->call('PostsTableSeeder');
+        $this->call('ProjectsTableSeeder');
+    }
 
 }
