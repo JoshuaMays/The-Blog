06bc988ec0f9480d0d8a9ca3efa62ccb6e89bfe1
haydogsup <joshua.r.mays@gmail.com>
2014-10-13 14:50:34 -0500

    removing table truncate

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -3,11 +3,13 @@
 class UsersTableSeeder extends Seeder {
     public function run()
     {
-        DB::table('users')->truncate();
-        
         $user = new User();
         $user->email = 'admin@codeup.com';
         $user->password = 'lololol';
+        $user->username = 'admin';
+        $user->image_path = 'img/admin.jpg';
+        $user->first_name = 'testfirst';
+        $user->last_name = 'testlast';
         $user->save();
     }
 }
