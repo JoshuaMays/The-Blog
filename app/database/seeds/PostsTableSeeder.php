99e2afb8daa59e31108dd7e24f3b41550649cba9
haydogsup <joshua.r.mays@gmail.com>
2014-10-13 14:30:55 -0500

    adding fake createdat and updatedat seeds

--- app/database/seeds/PostsTableSeeder.php
+++ app/database/seeds/PostsTableSeeder.php
@@ -5,15 +5,20 @@ Class PostsTableSeeder extends Seeder {
     {
         $faker = Faker\Factory::create();
 
-        DB::table('posts')->truncate();
-        
         // GENERATE RANDOM NUMBER OF SENTENCES IN FAKER->PARAGRAPH
         $rand = mt_rand(4,20);
-        
+
+        $user = User::first();
+
         foreach(range(1, 30) as $index) {
+            $updatedAt = $faker->dateTimeThisYear();
+            $createdAt = $faker->dateTimeThisYear($updatedAt);
             Post::create([
                 'title' => $faker->catchPhrase,
-                'content' => $faker->paragraph($rand)
+                'content' => $faker->paragraph($rand),
+                'updated_at' => $updatedAt,
+                'created_at' => $createdAt,
+                'user_id' => $user->id
             ]);
         }
     }
