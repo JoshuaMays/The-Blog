c711d96af77ffe0c7fd0eb4650dc60f4cb8263d8
haydogsup <joshua.r.mays@gmail.com>
2014-10-14 13:10:15 -0500

    moving soft deleting trait down to base model

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -1,8 +1,10 @@
 <?php
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class BaseModel extends Eloquent {
+    use SoftDeletingTrait;
 
     // FORMATS UTC TIMESTAMPS TO CHICAGO TIMEZONE
     protected function formatUTCToChicago($value)
