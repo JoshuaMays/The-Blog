95c538572781dc5054e8929a6e1e358968dd9d01
haydogsup <joshua.r.mays@gmail.com>
2014-10-15 19:32:58 -0500

    added image input

--- app/views/posts/form.blade.php
+++ app/views/posts/form.blade.php
@@ -10,3 +10,9 @@
     {{ Form::textarea('content', Input::old('content'), array('class' => 'form-control', 'placeholder' => 'Your blog post text here', 'rows' => '15')) }}
     {{ $errors->first('content', '<span class="alert alert-danger alert-dismissible help-block"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
 </div>
+
+<div class="form-group">
+    {{ Form::label('image','Add an Image') }}
+    <span class="btn btn-default btn-xs btn-file btn-outline">BROWSE{{ Form::file('image', null, array('class' => 'form-control')) }}</span>
+    {{ $errors->first('image', '<span class="alert alert-danger alert-dismissible help-block"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
+</div>
