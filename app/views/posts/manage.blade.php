3a3feb3c631e31148876886a3e6a537497d3932f
haydogsup <joshua.r.mays@gmail.com>
2014-10-15 19:34:05 -0500

    Adding admin views for managing and mass managing of posts.

---
+++ app/views/posts/manage.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Date Posted</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($posts as $post)
+            <tr id="post-row-{{{ $post->id }}}">
+                <td>{{{ $post->title }}}</td>
+                <td>{{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</td>
+                <td>{{{ $post->created_at }}}</td>
+                <td>
+                    <div class="btn-group">
+                        {{ link_to_action('PostsController@edit', 'Edit', $post->id, array('class' => 'btn btn-default')) }}
+                        <button type="button" class="btn btn-danger delete-btn" data-post-id="{{{ $post->id }}}">Delete</button>
+                    </div>
+                </td>
+            </tr>
+            @empty
+                
+            @endforelse
+        </tbody>
+    </table>
+
+@stop
+
+@section('bottom-script')
+    <script>
+        var csrfToken = "{{{ Session::get('_token') }}}";
+    
+        $(".delete-btn").click(function()
+        {
+            var postId = $(this).data('post-id');
+            
+            if(confirm('Are you sure that you want to delete the post?')) {
+                $.ajax({
+                    url: '/posts/' + postId,
+                    type:'post',
+                    data: {
+                        _token: csrfToken,
+                        _method: 'delete'
+                    },
+                    success: function(data) {
+                        if (data.success) {
+                            alert(data.message);
+                            $("#post-row-" + postId).fadeOut(1000);
+                        }
+                    }
+                });
+            }
+        });
+    </script>
+    
+@stop
---
+++ app/views/posts/massmanage.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th id="deleteTableHead"><button type="button" class="btn btn-xs btn-danger delete-btn">Delete</button></th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Date Posted</th>
+                <th>Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($posts as $post)
+            <tr id="post-row-{{{ $post->id }}}">
+                <td class="mass-delete-check">{{ Form::checkbox('delete-check', $post->id, false, array('class' => 'delete-check') ) }}
+                <td id="post-title-{{{ $post->id }}}">{{{ $post->title }}}</td>
+                <td>{{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</td>
+                <td>{{{ $post->created_at }}}</td>
+                <td>
+                    <div class="btn-group">
+                        {{ link_to_action('PostsController@edit', 'Edit', $post->id, array('class' => 'btn btn-default')) }}
+                    </div>
+                </td>
+            </tr>
+            @empty
+                
+            @endforelse
+        </tbody>
+
+    </table>
+
+    {{  $posts->appends(Request::only(['tag','search']))->links() }}
+@stop
+
+@section('bottom-script')
+
+<script>
+    var csrfToken = "{{{ Session::get('_token') }}}";
+    var fadeTimer = 0;
+    $(".delete-btn").click(function()
+    {
+        $(".delete-check:checked").each(function(){
+
+            var postId = $(this).val();
+            var postTitle = $("#post-title-" + postId).text();
+            fadeTimer += 750;
+            
+            if(confirm('Are you sure that you want to delete the post?')) {
+                $.ajax({
+                    url: '/posts/' + postId,
+                    type: 'post',
+                    data: {
+                        _token: csrfToken,
+                        _method: 'delete'
+                    },
+                    success: function(data) {
+                        if (data.success) {
+                            alert(data.message);
+                            $("#post-row-" + postId).fadeOut(fadeTimer);
+                        }
+                    }
+                });
+            }
+        });
+    });
+</script>
+
+@stop
